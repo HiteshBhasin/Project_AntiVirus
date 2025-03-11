faef9b81bfe3ce9531aa0b9a2b9cd64c167c3117
Hitesh Bhasin <77706673+HiteshBhasin@users.noreply.github.com>
2025-03-11 17:30:24 +0000

    new commit

---
+++ anitvirus_fourth_gen.py
@@ -0,0 +1,25 @@
+import yara
+import hashlib
+
+'''
+purpose of this script will be to use hashlib first 
+and return the hexvalue once that is done we will double check the 
+file with yara rules to make sure it is legit. 
+'''
+#----------------------------------------------------------------------
+'''
+ here we are taking the path of a file and ready 4906 chinks as a time. 
+ we can choose any buffer_size a chunk to read the file. While 
+ statement make sure that there is a data to read, once there is no more data the while loop breaks.
+ once all done the func return the hexdigit. 
+'''
+
+
+buffer_size = 4906
+def has_check(file_path):
+    sha256 = hashlib.sha256()
+    with open (file_path, 'rb') as raw_bin_file:
+        while chunk:= raw_bin_file.read(buffer_size):
+            sha256.update(chunk)
+    return sha256.hexdigest()
+    

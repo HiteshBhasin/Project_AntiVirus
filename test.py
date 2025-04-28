08751f5b82d379c23f916b47e26d776f96eda26a
Hitesh Bhasin <77706673+HiteshBhasin@users.noreply.github.com>
2025-04-28 21:10:24 +0000

    new commit

--- test.py
+++ test.py
@@ -1,2 +1,20 @@
-def addRungs(self, rungs: List[int], dist: int) -> int:
-    
+# def addRungs(self, rungs: List[int], dist: int) -> int:
+#     count =0
+#     if rungs[0]>dist:
+#         m=rungs[0]
+#         m=(m-1)
+#         count+=m        
+#     for i in range(len(rungs)-1):
+#         k = (rungs[i+1] - rungs[i])
+#         if k>dist:
+            
+
+def maximumNumber(self, num: str, change: List[int]) -> str:           
+            flag = False
+            res = " "
+            
+            for digit in num:
+                d = int(digit)
+                if change[d]>d:
+            
+        
